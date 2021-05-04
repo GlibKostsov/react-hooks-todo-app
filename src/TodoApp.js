89e94e729b46b18107b7a8fd4efd12be722915b1
Glib Kostsov <glibkostsov@gmail.com>
2021-05-04 14:29:55 +0200

    add removeTodo, toggleTodo Method(s) & implement uuid

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,6 +6,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Grid from '@material-ui/core/Grid'
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
+import { v4 as uuid } from 'uuid'
 
 export default function TodoApp() {
   const initialTodos = [
@@ -27,8 +28,21 @@ export default function TodoApp() {
   ]
   const [todos, setTodos] = useState(initialTodos)
   const addTodo = (newTodoText) => {
-    setTodos([...todos, { id: 4, task: newTodoText, completed: false }])
+    setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }])
   }
+
+  const removeTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId)
+    setTodos(updatedTodos)
+  }
+
+  const toggleTodo = (todoId) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+    )
+    setTodos(updatedTodos)
+  }
+
   return (
     <Paper
       style={{
@@ -48,7 +62,11 @@ export default function TodoApp() {
       <Grid container justify='center' style={{ marginTop: '1rem' }}>
         <Grid item xs={11} md={8} lg={4}>
           <TodoForm addTodo={addTodo} />
-          <TodoList todos={todos} />
+          <TodoList
+            todos={todos}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+          />
         </Grid>
       </Grid>
     </Paper>
